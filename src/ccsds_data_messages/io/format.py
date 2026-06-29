a58fec8f14f49ed5e85e4433f51f62ba978bc105
Max <maximilian.niquet@loftorbital.com>
2026-06-29 20:16:35 +0000

    feat(io): add format types

---
+++ src/ccsds_data_messages/io/format.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+from enum import StrEnum
+
+
+class MessageFormat(StrEnum):
+    KVN = "kvn"
+    XML = "xml"
+
+
+class MessageType(StrEnum):
+    OEM = "oem"
+    OMM = "omm"
+    OPM = "opm"
+    OCM = "ocm"
